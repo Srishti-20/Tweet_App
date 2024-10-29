778ea0dabcd9b9d7435b28237877c04b52e9e9d6
Srishti Gupta <93029264+Srishti-20@users.noreply.github.com>
2024-10-29 22:21:24 +0530

    Initial commit

---
+++ TweetHQ/tweet/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render
+from .models import Tweet
+from .forms import TweetForm, UserRegistrationForm
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.db.models import Q
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+# list all tweets
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request, 'tweet_list.html', {'tweets': tweets})
+
+@login_required
+# create tweets
+def tweet_create(request):
+    if request.method == "POST":
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm()
+    return render(request, 'tweet_form.html', {'form': form})
+
+@login_required
+# edit your tweet
+def tweet_edit(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk = tweet_id, user=request.user)
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES, instance = tweet)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm(instance=tweet)
+    return render(request, 'tweet_form.html', {'form': form})
+
+@login_required
+# Delete your tweet 
+def tweet_delete(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+    if request.method == 'POST':
+        tweet.delete()
+        return redirect('tweet_list')
+    return render(request, 'tweet_confirm_delete.html', {'tweet': tweet})
+
+# User Resgister
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(request, user)
+            return redirect('tweet_list')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+# Search the Tweet
+def tweet_search(request):
+    query = request.GET.get('q')
+    if query:
+        tweets = Tweet.objects.filter(
+            Q(text__icontains=query) | Q(user__username__icontains=query)
+        ).order_by('-created_at')
+    else:
+        tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request, 'tweet_search.html', {'tweets': tweets})
